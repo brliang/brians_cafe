3e891f5f8eedcdbb81a0cd92a9d92f03ff4efabc
Brian Liang <Brian@Brians-Mac-Pro.local>
2019-12-28 13:38:33 -0500

    H1 styles changed

--- src/components/body.js
+++ src/components/body.js
@@ -4,18 +4,18 @@ import PropTypes from "prop-types"
 
 import pages from "../constants/global"
 import StyledBody from "../styled_components/body"
+import H1 from "../styled_components/h1"
 import Paragraph from "../styled_components/paragraph"
 import BrianSVG from "../svg/brian.svg"
 import FirstNameSVG from "../svg/firstName.svg"
 import LastNameSVG from "../svg/lastName.svg"
-import QuillSVG from "../svg/quill.svg"
 
 const Body = ({ pageTitle }) => {
   switch (pageTitle) {
     case pages.INDEX:
       return (
         <StyledBody>
-          <h1>Hi and welcome!</h1>
+          <H1>Hi and welcome!</H1>
           <BrianSVG id="brian" />
           <Paragraph>
             I'm an incoming pebble, and aspiring rock-star at{" "}
@@ -38,13 +38,13 @@ const Body = ({ pageTitle }) => {
     case pages.THOUGHTS:
       return (
         <StyledBody>
-          <h1>Thoughts... Coming soon!</h1>
+          <H1>Thoughts... Coming soon!</H1>
         </StyledBody>
       )
     default:
       return (
         <StyledBody>
-          <h1>Currently under construction</h1>
+          <H1>Currently under construction</H1>
         </StyledBody>
       )
   }
