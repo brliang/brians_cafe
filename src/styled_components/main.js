7fc680b8815ccc6b6a2b75a1076d975932036996
Brian Liang <bliang529@gmail.com>
2020-01-19 02:11:35 +0000

    Test 6

--- src/styled_components/main.js
+++ src/styled_components/main.js
@@ -1,28 +1,7 @@
 import styled from "styled-components"
 
 const Main = styled.main`
-  margin: 2vh auto 8vh;
-  max-width: 30vw;
 
-  @media screen and (min-width: 1800px) {
-    max-width: 20vw;
-  }
-
-  @media screen and (max-width: 800px) {
-    max-width: 50vw;
-  }
-
-  @media screen and (max-width: 700px) {
-    max-width: 65vw;
-  }
-
-  @media screen and (max-width: 600px) {
-    max-width: 80vw;
-  }
-
-  @media screen and (max-width: 500px) {
-    max-width: 95vw;
-  }
 `
 
 export default Main
