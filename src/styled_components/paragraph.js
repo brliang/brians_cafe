4cf70c7f74cead1289fa665a055a5ea2e9773eca
Brian Liang <bliang529@gmail.com>
2020-01-19 02:26:44 +0000

    DNFOSFS

--- src/styled_components/paragraph.js
+++ src/styled_components/paragraph.js
@@ -5,13 +5,6 @@ import global from "../styles/global"
 const { colors, font_sizes, font_weights } = global
 
 const Paragraph = styled.p`
-  background-color: ${colors.tertiary_color};
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 2px 4px 0 rgba(0, 0, 0, 0.2);
-  font-size: ${font_sizes.s};
-  font-weight: ${font_weights.light};
-  line-height: 1.25;
-  padding: 20px;
-  text-align: left;
 `
 
 export default Paragraph
