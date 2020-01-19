d12f1cec4cd4333ae31aefcc9495425011a9d1c0
Brian Liang <bliang529@gmail.com>
2020-01-19 02:02:47 +0000

    Test 4

--- src/styled_components/paragraph.js
+++ src/styled_components/paragraph.js
@@ -6,7 +6,6 @@ const { colors, font_sizes, font_weights } = global
 
 const Paragraph = styled.p`
   background-color: ${colors.tertiary_color};
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.1), 0 2px 4px 0 rgba(0, 0, 0, 0.2);
   font-size: ${font_sizes.s};
   font-weight: ${font_weights.light};
   line-height: 1.25;
