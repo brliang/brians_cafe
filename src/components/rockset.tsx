4a7a529bc98d9ab4348f9994fe1cbe641f610dde
Brian Liang <brian@rockset.com>
2020-09-13 20:57:10 -0700

    minus key

--- src/components/rockset.tsx
+++ src/components/rockset.tsx
@@ -1,66 +1,66 @@
-import * as React from 'react';
-import rocksetConfigure from 'rockset';
-import { Button, Textarea } from '../styled_components/index';
+// import * as React from 'react';
+// import rocksetConfigure from 'rockset';
+// import { Button, Textarea } from '../styled_components/index';
 
-export const Rockset = () => {
-  const rockset = rocksetConfigure(
-    'GHLfQ2fiLz5NGgh6Wb6aIKT0vlb0PvIdBQkc5LaVeKZarR4J8Ulx5B2wxwXW2KOj',
-    'https://api.rs2.usw2.rockset.com'
-  );
+// export const Rockset = () => {
+//   const rockset = rocksetConfigure(
+//     'key?',
+//     'https://api.rs2.usw2.rockset.com'
+//   );
 
-  return (
-    <div>
-      <Button
-        onClick={() => {
-          rockset.documents.addDocuments('drinks', 'Coffee', {
-            data: [{ roast: 'light' }],
-          });
-        }}
-      >
-        Write API
-      </Button>
-      {/* <Button
-        onClick={() => {
-          fetch('/.netlify/functions/coffee', {
-            method: 'GET',
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-          }).then(response =>
-            response.text().then(data => setResponseData(data))
-          );
-        }}
-      >
-        Get Coffee
-      </Button>
-      <Button
-        onClick={() => {
-          fetch('/.netlify/functions/coffee', {
-            method: 'POST',
-            body: JSON.stringify('new FormData'),
-          }).then(response => response.json());
-        }}
-      >
-        Coffee
-      </Button>
+//   return (
+//     <div>
+//       <Button
+//         onClick={() => {
+//           rockset.documents.addDocuments('drinks', 'Coffee', {
+//             data: [{ roast: 'light' }],
+//           });
+//         }}
+//       >
+//         Write API
+//       </Button>
+//       {/* <Button
+//         onClick={() => {
+//           fetch('/.netlify/functions/coffee', {
+//             method: 'GET',
+//             headers: {
+//               'Content-type': 'application/json; charset=UTF-8',
+//             },
+//           }).then(response =>
+//             response.text().then(data => setResponseData(data))
+//           );
+//         }}
+//       >
+//         Get Coffee
+//       </Button>
+//       <Button
+//         onClick={() => {
+//           fetch('/.netlify/functions/coffee', {
+//             method: 'POST',
+//             body: JSON.stringify('new FormData'),
+//           }).then(response => response.json());
+//         }}
+//       >
+//         Coffee
+//       </Button>
 
-      <Button
-        onClick={() => {
-          fetch('/.netlify/functions/tea', {
-            method: 'POST',
-            headers: {
-              'Content-type': 'application/json; charset=UTF-8',
-            },
-            body: JSON.stringify([
-              {
-                type: 'green',
-              },
-            ]),
-          });
-        }}
-      >
-        Tea
-      </Button> */}
-    </div>
-  );
-};
+//       <Button
+//         onClick={() => {
+//           fetch('/.netlify/functions/tea', {
+//             method: 'POST',
+//             headers: {
+//               'Content-type': 'application/json; charset=UTF-8',
+//             },
+//             body: JSON.stringify([
+//               {
+//                 type: 'green',
+//               },
+//             ]),
+//           });
+//         }}
+//       >
+//         Tea
+//       </Button> */}
+//     </div>
+//   );
+// };
